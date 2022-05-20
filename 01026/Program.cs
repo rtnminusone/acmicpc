481a62bc044b745ba8b9e15fefabbc63868bbbd6
returnminusone <returnminusone@outlook.com>
2022-05-21 07:06:20 +0900

    first commit

---
+++ 01026/Program.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable CS8602, CS8604
+
+int N = int.Parse(Console.ReadLine());
+
+int[] A = new int[N];
+int[] B = new int[N];
+
+string[] buf = Console.ReadLine().Split();
+
+for (int i = 0; i < N; i++)
+{
+    A[i] = int.Parse(buf[i]);
+}
+
+Array.Clear(buf);
+
+buf = Console.ReadLine().Split();
+
+for (int i = 0; i < N; i++)
+{
+    B[i] = int.Parse(buf[i]);
+}
+
+Array.Sort(A);
+Array.Sort(B);
+Array.Reverse(B);
+
+int S = 0;
+
+for (int i = 0; i < N; i++)
+{
+    S += A[i] * B[i];
+}
+
+Console.WriteLine(S);
