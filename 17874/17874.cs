2a6c36205b0173505c4cb2981e41852b08b08454
returnminusone <returnminusone@outlook.com>
2022-05-22 04:14:47 +0900

    add 17874

---
+++ 17874/17874.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+string[] buf = Console.ReadLine().Split();
+int M = int.Parse(buf[0]) / 2;
+
+if (M >= int.Parse(buf[1]))
+{
+    if (M >= int.Parse(buf[2])) Console.WriteLine((int.Parse(buf[0]) - int.Parse(buf[1])) * (int.Parse(buf[0]) - int.Parse(buf[2])) * 4);
+    else Console.WriteLine((int.Parse(buf[0]) - int.Parse(buf[1])) * int.Parse(buf[2]) * 4);
+}
+else
+{
+    if (M >= int.Parse(buf[2])) Console.WriteLine(int.Parse(buf[1]) * (int.Parse(buf[0]) - int.Parse(buf[2])) * 4);
+    else Console.WriteLine(int.Parse(buf[1]) * int.Parse(buf[2]) * 4);
+}
