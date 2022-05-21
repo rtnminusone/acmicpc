b2fa8351738fba6c5e259a97fa97d45f8e45de1c
returnminusone <returnminusone@outlook.com>
2022-05-22 03:48:05 +0900

    add 13225

---
+++ 13225/13225.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System.Text;
+using System;
+
+StringBuilder sb = new StringBuilder();
+
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    int M = int.Parse(Console.ReadLine());
+    int R = 0;
+    for (int j = 1; j < M; j++)
+    {
+        if (M % j == 0) R++;
+    }
+    sb.AppendFormat("{0} {1}\n", M, R + 1);
+}
+
+Console.WriteLine(sb);
