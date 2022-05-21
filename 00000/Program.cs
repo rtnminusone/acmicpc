102beb737fafefd226e0d79e4e8f220d4a7aa037
returnminusone <returnminusone@outlook.com>
2022-05-21 16:39:04 +0900

    00000

--- 00000/Program.cs
+++ 00000/Program.cs
@@ -1,5 +1,9 @@
-﻿string s = Console.ReadLine();
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+string s = Console.ReadLine();
 string[] ss = s.Split();
+
 int a = int.Parse(ss[0]);
 int b = int.Parse(ss[1]);
-Console.WriteLine(a+b);
+
+Console.WriteLine(a + b);
