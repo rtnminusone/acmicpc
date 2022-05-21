b4877657573b2b81c68fcf60ed0bfa2d7559b158
returnminusone <returnminusone@outlook.com>
2022-05-22 02:25:56 +0900

    add 11549

---
+++ 11549/11549.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System.Text;
+using System;
+
+string N = Console.ReadLine();
+string[] buf = Console.ReadLine().Split();
+int R = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    if (buf[i].Equals(N)) R++;
+}
+
+Console.WriteLine(R);
