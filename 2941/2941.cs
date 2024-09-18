fc135e0e3b46bde80b738c6ddb7fe1158f9b7418
returnminusone <returnminusone@outlook.com>
2024-09-18 12:36:39 +0900

    add 2941

---
+++ 2941/2941.cs
@@ -0,0 +1,33 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System.Text;
+using System;
+
+String N = Console.ReadLine();
+int count = 0;
+
+for (int i = 0; i < N.Length; i++)
+{
+	if (N[i] == 'c')
+	{
+		if (N.Length - 1 >= i + 1 && N[i + 1] == '=') i++;
+		else if (N.Length - 1 >= i + 1 && N[i + 1] == '-') i++;
+	}
+	else if (N[i] == 'd')
+	{
+		if (N.Length - 1 >= i + 1 && N[i + 1] == '-') i++;
+		else if (N.Length - 1 >= i + 2 && N.Substring(i, 3).Equals("dz=")) i += 2;
+	}
+	else if (N[i] == 'l' || N[i] == 'n')
+	{
+		if (N.Length - 1 >= i + 1 && N[i + 1] == 'j') i++;
+	}
+	else if (N[i] == 's' || N[i] == 'z')
+	{
+		if (N.Length - 1 >= i + 1 && N[i + 1] == '=') i++;
+	}
+
+	count++;
+}
+
+Console.WriteLine(count);
