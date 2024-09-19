a8fedce2e8d0144bc18a7f7d42f07a6ba1062795
returnminusone <returnminusone@outlook.com>
2024-09-19 12:39:22 +0900

    add 2751

---
+++ 2751/2751.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+int N = int.Parse(Console.ReadLine());
+int[] list = new int[N];
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < N; i++)
+{
+	list[i] = int.Parse(Console.ReadLine());
+}
+
+Array.Sort(list);
+
+for (int i = 0; i < N; i++)
+{
+	sb.Append(list[i] + "\n");
+}
+
+Console.WriteLine(sb);
