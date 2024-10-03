73f43fd0a0ee102e65f40dda5aa26eaf2b16c76c
returnminusone <returnminusone@outlook.com>
2024-10-03 19:19:22 +0900

    add 2566

---
+++ 2566/2566.cs
@@ -0,0 +1,31 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		int[] p = new int[2]{ 1, 1 };
+		int r = 0;
+
+		for (int i = 0; i < 9; i++)
+		{
+			string[] b = Console.ReadLine().Split(' ');
+
+			for (int j = 0; j < 9; j++)
+			{
+				if (r < int.Parse(b[j]))
+				{
+					r = int.Parse(b[j]);
+					p[0] = i + 1;
+					p[1] = j + 1;
+				}
+			}
+		}
+
+		Console.WriteLine(r);
+		Console.WriteLine(p[0] + " " + p[1]);
+	}
+}
