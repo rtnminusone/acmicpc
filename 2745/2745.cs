d75ed71d73e8d84d2cee31dff3048a7ca33c7620
returnminusone <returnminusone@outlook.com>
2024-10-07 12:50:48 +0900

    add 2745

---
+++ 2745/2745.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static long power(int c, int pow)
+	{
+		long r = 1;
+
+		for (int i = 0; i < pow; i++)
+		{
+			r *= c;
+		}
+
+		return r;
+	}
+
+	public static void Main(string[] args)
+	{
+		string[] s = Console.ReadLine().Split(' ');
+		int B = int.Parse(s[1]);
+		long R = 0;
+		int pow = 0;
+
+		for (int i = s[0].Length - 1; i >= 0; i--)
+		{
+			if (0 <= s[0][i] - '0' && s[0][i] - '0' <= 9) R += (s[0][i] - '0') * power(B, pow++);
+			else R += (s[0][i] - '7') * power(B, pow++);
+		}
+
+		Console.WriteLine(R);
+	}
+}
