54681f70965d111959a55d896c5fae988d1035a4
returnminusone <returnminusone@outlook.com>
2024-10-09 12:59:42 +0900

    add 2903

---
+++ 2903/2903.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		int N = int.Parse(Console.ReadLine());
+		int K = 3;
+
+		for (int i = 1; i < N; i++)
+		{
+			K = 2 * K - 1;
+		}
+
+		Console.WriteLine(K * K);
+	}
+}
