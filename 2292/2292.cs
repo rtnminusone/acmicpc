7165255d08e715a967f1a25c35376dfed2c2b222
returnminusone <returnminusone@outlook.com>
2024-10-10 12:20:09 +0900

    add 2292

---
+++ 2292/2292.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		int N = int.Parse(Console.ReadLine());
+		int K = 1;
+		int C = 1;
+
+		while (K < N)
+		{
+			K += C * 6;
+			C++;
+		}
+
+		Console.WriteLine(C);
+	}
+}
