a633d598fba8af8eb0c2711e378454c318faefe6
returnminusone <returnminusone@outlook.com>
2024-10-16 19:01:33 +0900

    add 15894

---
+++ 15894/15894.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine(long.Parse(Console.ReadLine()) * 4);
+	}
+}
