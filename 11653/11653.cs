2701df097df05d7aa5456714841175faf259c5bd
returnminusone <returnminusone@outlook.com>
2024-10-16 12:39:07 +0900

    add 11653

---
+++ 11653/11653.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		StringBuilder sb = new StringBuilder();
+		int N = int.Parse(Console.ReadLine());
+		int K = 2;
+
+		while (N != 1 && N >= K)
+		{
+			if (N % K == 0)
+			{
+				sb.AppendLine(K + "");
+				N /= K;
+			}
+			else K++;
+		}
+
+		Console.WriteLine(sb);
+	}
+}
