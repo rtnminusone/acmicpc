4f3b103e933f27967f564ce20e1ce7afe09d8419
returnminusone <returnminusone@outlook.com>
2024-10-17 12:28:41 +0900

    add 10101

---
+++ 10101/10101.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		int[] X = new int[3];
+		int R = 0;
+
+		for (int i = 0; i < 3; i++)
+		{
+			X[i] = int.Parse(Console.ReadLine());
+			R += X[i];
+		}
+
+		if (R != 180) Console.WriteLine("Error");
+		else if (X[0] == X[1] && X[1] == X[2]) Console.WriteLine("Equilateral");
+		else if (X[0] == X[1] || X[0] == X[2] || X[1] == X[2]) Console.WriteLine("Isosceles");
+		else Console.WriteLine("Scalene");
+	}
+}
