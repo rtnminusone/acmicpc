085432ac560becc8ed16da93d70929fda6c86b9b
returnminusone <returnminusone@outlook.com>
2024-10-22 12:39:26 +0900

    add 24267

---
+++ 24267/24267.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+
+long N = long.Parse(Console.ReadLine());
+
+if (N == 1 || N == 2) Console.WriteLine(0);
+else Console.WriteLine(N * (N - 1) * (N - 2) / 6);
+Console.WriteLine(3);
