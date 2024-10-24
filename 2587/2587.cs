db7faed81b8185696f994d395f15cac2d8e30508
returnminusone <returnminusone@outlook.com>
2024-10-24 12:57:12 +0900

    add 2587

---
+++ 2587/2587.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+
+int R = 0;
+int[] K = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+	K[i] = int.Parse(Console.ReadLine());
+	R += K[i];
+}
+
+Array.Sort(K);
+Console.WriteLine(R / 5);
+Console.WriteLine(K[2]);
