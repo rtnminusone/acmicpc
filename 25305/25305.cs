00e187bbc190102d68f4691eb1c6a03e986c2fc1
returnminusone <returnminusone@outlook.com>
2024-10-24 19:54:45 +0900

    add 25305

---
+++ 25305/25305.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+
+string[] S = Console.ReadLine().Split(' ');
+int N = int.Parse(S[0]);
+int K = int.Parse(S[1]);
+int[] R = new int[N];
+S = Console.ReadLine().Split(' ');
+
+for (int i = 0; i < N; i++)
+{
+	R[i] = int.Parse(S[i]);
+}
+
+Array.Sort(R);
+Console.WriteLine(R[N - K]);
