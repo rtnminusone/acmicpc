ccce9a3792f4ffc052c2c430ca00383801eace52
returnminusone <returnminusone@outlook.com>
2024-10-30 13:00:41 +0900

    add 10773

---
+++ 10773/10773.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+
+int N = int.Parse(Console.ReadLine());
+long R = 0;
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < N; i++)
+{
+	int K = int.Parse(Console.ReadLine());
+	if (K == 0)
+	{
+		R -= stack.Pop();
+	}
+	else
+	{
+		stack.Push(K);
+		R += K;
+	}
+}
+
+Console.WriteLine(R);
