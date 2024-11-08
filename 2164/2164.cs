7858a867aebf09eec493b07891f974173260930b
returnminusone <returnminusone@outlook.com>
2024-11-08 12:20:47 +0900

    add 2164

---
+++ 2164/2164.cs
@@ -0,0 +1,19 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+
+Queue<int> Q = new Queue<int>();
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+	Q.Enqueue(i);
+}
+
+while (Q.Count != 1)
+{
+	Q.Dequeue();
+	Q.Enqueue(Q.Dequeue());
+}
+
+Console.WriteLine(Q.Dequeue());
