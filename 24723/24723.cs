816cec88d58e85c70ad3a7f22c45803eef164198
returnminusone <returnminusone@outlook.com>
2024-11-19 12:08:19 +0900

    add 24723

---
+++ 24723/24723.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+
+int N = int.Parse(Console.ReadLine());
+int K = 1;
+
+for (int i = 0; i < N; i++)
+{
+	K *= 2;
+}
+
+Console.WriteLine(K);
