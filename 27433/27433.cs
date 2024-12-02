3e7bded7e13535b31d648f567d6b41a9804d0fd8
returnminusone <returnminusone@outlook.com>
2024-12-02 12:27:24 +0900

    add 27433

---
+++ 27433/27433.cs
@@ -0,0 +1,20 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+
+class Program
+{
+	public static long F(int K)
+	{
+		if (K == 1) return 1;
+		return K * F(K - 1);
+	}
+
+	public static void Main(string[] args)
+	{
+		int N = int.Parse(Console.ReadLine());
+
+		if (N == 0) Console.WriteLine(1 + "");
+		else Console.WriteLine(F(N) + "");
+	}
+}
