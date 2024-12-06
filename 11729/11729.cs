495fe0683f8ec02424e40fd203a3d732d3ec9c46
returnminusone <returnminusone@outlook.com>
2024-12-06 12:24:08 +0900

    add 11729

---
+++ 11729/11729.cs
@@ -0,0 +1,36 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static int count = 0;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void hanoi(int n, int from, int mid, int to)
+	{
+		if (n == 1)
+		{
+			count++;
+			sb.AppendLine(from + " " + to);
+		}
+		else
+		{
+			hanoi(n - 1, from, to, mid);
+			count++;
+			sb.AppendLine(from + " " + to);
+			hanoi(n - 1, mid, from, to);
+		}
+	}
+
+	public static void Main(string[] args)
+	{
+		int N = int.Parse(Console.ReadLine());
+
+		hanoi(N, 1, 2, 3);
+
+		Console.WriteLine(count);
+		Console.WriteLine(sb);
+	}
+}
