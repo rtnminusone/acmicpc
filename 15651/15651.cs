7a9547c13ebe86a60d56557f373f3a3e4752610d
returnminusone <returnminusone@outlook.com>
2024-12-09 13:54:14 +0900

    add 15651

---
+++ 15651/15651.cs
@@ -0,0 +1,48 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System;
+using System.Text;
+
+class Program
+{
+	public static int N;
+	public static int K;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void back_tracking(int depth, int[] result, bool[,] visited)
+	{
+		if (depth == K)
+		{
+			sb.AppendLine(string.Join(" ", result));
+			return;
+		}
+		else
+		{
+			for (int i = 1; i <= N; i++)
+			{
+				if (!visited[depth, i])
+				{
+					result[depth] = i;
+					visited[depth, i] = true;
+					back_tracking(depth + 1, result, visited);
+					visited[depth, i] = false;
+				}
+			}
+		}
+	}
+
+	public static void Main(string[] args)
+	{
+		string[] S = Console.ReadLine().Split(' ');
+		N = int.Parse(S[0]);
+		K = int.Parse(S[1]);
+		bool[,] visited = new bool[K, N + 1];
+		int[] result = new int[K];
+		Array.Clear(visited, 0, visited.Length);
+		Array.Fill(result, 0);
+
+		back_tracking(0, result, visited);
+
+		Console.WriteLine(sb);
+	}
+}
