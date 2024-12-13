0c10f0e5d3cea16045fde5b7ff80c68192f57654
returnminusone <returnminusone@outlook.com>
2024-12-13 12:14:28 +0900

    add 24313

---
+++ 24313/24313.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604, CS8618
+
+using System;
+using System.Text;
+
+string[] S = Console.ReadLine().Split();
+int a1 = int.Parse(S[0]);
+int a0 = int.Parse(S[1]);
+int c = int.Parse(Console.ReadLine());
+int n0 = int.Parse(Console.ReadLine());
+
+bool B = true;
+for (int i = n0; i <= 100000; i++)
+{
+	if (a1 * i + a0 > c * i)
+	{
+		B = false;
+		break;
+	}
+}
+
+if (B) Console.WriteLine(1);
+else Console.WriteLine(0);
