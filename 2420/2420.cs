d6107cc4ccf3a14df4227d97589c2d41cf37ed5d
returnminusone <returnminusone@outlook.com>
2024-12-18 14:02:23 +0900

    add 2420

---
+++ 2420/2420.cs
@@ -0,0 +1,6 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8604, CS8618
+
+using System;
+
+string[] S = Console.ReadLine().Split(' ');
+Console.WriteLine(Math.Abs(long.Parse(S[0]) - long.Parse(S[1])));
