7e76a1b7cade04c3d72ef98cd28e82e7f7482929
returnminusone <returnminusone@outlook.com>
2024-12-18 14:33:16 +0900

    add 2744

---
+++ 2744/2744.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8604, CS8618
+
+using System;
+
+char[] C = Console.ReadLine().ToCharArray();
+
+for (int i = 0; i < C.Length; i++)
+{
+	if ('A' <= C[i] && C[i] <= 'Z') C[i] = (char)(C[i] + 'a' - 'A');
+	else C[i] = (char)(C[i] + 'A' - 'a');
+}
+
+Console.WriteLine(new string(C));
