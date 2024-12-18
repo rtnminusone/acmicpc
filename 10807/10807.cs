31b3de37b32b2dbd42e3367a4e219cb960493d61
returnminusone <returnminusone@outlook.com>
2024-12-18 14:16:07 +0900

    mod 10807

--- 10807/10807.cs
+++ 10807/10807.cs
@@ -1,5 +1,6 @@
-﻿#pragma warning disable CS8600, CS8602, CS8604
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8604
 
+/* pass case 1 start
 int i = int.Parse(Console.ReadLine());
 string[] s = Console.ReadLine().Split();
 int j = int.Parse(Console.ReadLine());
@@ -10,3 +11,10 @@ for (int l = 0; l < i; l++) {
 }
 
 Console.WriteLine(k);
+pass case 1 end */
+
+int N = int.Parse(Console.ReadLine());
+
+string[] S = Console.ReadLine().Split(' ');
+string V = Console.ReadLine();
+Console.WriteLine(S.Count(s => s == V));
