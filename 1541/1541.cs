486df222e9f25e3a4f9abe48c1af308889694263
returnminusone <returnminusone@outlook.com>
2025-01-30 16:48:18 +0900

    add 1541

---
+++ 1541/1541.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618
+
+using System;
+using System.Text;
+
+List<int> L = new List<int>();
+string S = Console.ReadLine();
+
+int R = 0;
+StringBuilder sb = new StringBuilder();
+foreach (char c in S)
+{
+	if (c == '-' || c == '+')
+	{
+		R += int.Parse(sb.ToString());
+		sb.Clear();
+		if (c == '-')
+		{
+			L.Add(R);
+			R = 0;
+		}
+	}
+	else sb.Append(c);
+}
+R += int.Parse(sb.ToString());
+L.Add(R);
+
+if (L.Count != 1) R = L[0];
+for (int i = 1; i < L.Count; i++)
+{
+	R -= L[i];
+}
+
+Console.WriteLine(R);
