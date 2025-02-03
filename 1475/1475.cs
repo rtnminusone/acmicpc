39516baf67ea73711ed716b5da2acef5e5c7ece8
returnminusone <returnminusone@outlook.com>
2025-02-03 12:47:40 +0900

    add 1475

---
+++ 1475/1475.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618
+
+using System;
+
+int[] T = new int[9];
+
+string S = Console.ReadLine();
+for (int i = 0; i < S.Length; i++)
+{
+	if ('0' <= S[i] && S[i] <= '8') T[S[i] - '0']++;
+	else T[6]++;
+}
+T[6] -= T[6] / 2;
+
+Array.Sort(T);
+
+Console.WriteLine(T[8]);
