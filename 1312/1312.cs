eea7f527599125b34b5d4e1cbc33ed39e62eb0ba
returnminusone <returnminusone@outlook.com>
2025-02-03 12:27:35 +0900

    add 1312

---
+++ 1312/1312.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618
+
+using System;
+
+string[] S = Console.ReadLine().Split();
+
+long A = long.Parse(S[0]);
+long B = long.Parse(S[1]);
+long N = long.Parse(S[2]);
+
+for (int i = 0; i < N; i++)
+{
+	A %= B;
+	A *= 10;
+}
+
+Console.WriteLine(A / B % 10);
