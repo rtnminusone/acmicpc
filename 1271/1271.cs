1f6c0e1d8cbe36f84745ac8e795f7d56e2546218
returnminusone <returnminusone@outlook.com>
2026-02-10 10:37:08 +0900

    add 1271

---
+++ 1271/1271.cs
@@ -0,0 +1,8 @@
+﻿#pragma warning disable CS8600, CS8602, CS8604
+
+using System.Numerics;
+
+string[] S = Console.ReadLine().Split();
+BigInteger N = BigInteger.Parse(S[0]);
+BigInteger M = BigInteger.Parse(S[1]);
+Console.WriteLine((N / M) + "\n" + (N % M));
