af3003bc4e782f9059f67f4697127d4754348219
returnminusone <returnminusone@outlook.com>
2026-02-10 10:08:28 +0900

    add 2845

---
+++ 2845/2845.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable CS8602
+
+string[] S = Console.ReadLine().Split();
+int N = int.Parse(S[0]) * int.Parse(S[1]);
+S = Console.ReadLine().Split();
+for (int i = 0; i < 5; i++)
+{
+	Console.Write(int.Parse(S[i]) - N + " ");
+}
