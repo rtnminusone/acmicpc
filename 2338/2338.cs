a71bc6c5cf1974653db58d15229da745aa3384f4
returnminusone <returnminusone@outlook.com>
2026-02-10 10:19:13 +0900

    add 2338

---
+++ 2338/2338.cs
@@ -0,0 +1,7 @@
+﻿#pragma warning disable CS8604
+
+using System.Numerics;
+
+BigInteger N = BigInteger.Parse(Console.ReadLine());
+BigInteger M = BigInteger.Parse(Console.ReadLine());
+Console.WriteLine((N + M) + "\n" + (N - M) + "\n" + (N * M));
