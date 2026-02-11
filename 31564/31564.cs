c55855b5bb36993c9116975c4b2eaaa3cd8ac37a
returnminusone <returnminusone@outlook.com>
2026-02-11 23:29:11 +0900

    add 31564

---
+++ 31564/31564.cs
@@ -0,0 +1,84 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8625
+
+using System;
+
+class Program
+{
+	public class Pos
+	{
+		public int x;
+		public int y;
+		public int w;
+
+		public Pos(int x, int y, int w)
+		{
+			this.x = x;
+			this.y = y;
+			this.w = w;
+		}
+	}
+
+	public static Pos Create(int x, int y, int w)
+	{
+		if (x < 0 || x >= N || y < 0 || y >= M) return null;
+		if (T[x, y] == 1) return null;
+		if (V[x, y]) return null;
+
+		return new Pos(x, y, w);
+	}
+
+	public static bool Push(Pos pos)
+	{
+		if (pos == null) return false;
+
+		Q.Enqueue(pos);
+		V[pos.x, pos.y] = true;
+
+		return true;
+	}
+
+	public static int BFS()
+	{
+		while (Q.Count > 0)
+		{
+			Pos p = Q.Dequeue();
+
+			if (p.x == N - 1 && p.y == M - 1) return p.w;
+
+			int start = p.x % 2 == 0 ? 0 : 6;
+
+			for (int i = start; i < start + 6; i++)
+			{
+				Push(Create(p.x + dx[i], p.y + dy[i], p.w + 1));
+			}
+		}
+
+		return -1;
+	}
+
+	public static int N, M;
+	public static int[,] T;
+	public static bool[,] V;
+	public static Queue<Pos> Q = new Queue<Pos>();
+
+	public static int[] dx = { -1, -1, 0, 0, 1, 1, -1, -1, 0, 0, 1, 1 };
+	public static int[] dy = { -1, 0, -1, 1, -1, 0, 0, 1, -1, 1, 0, 1 };
+
+	public static void Main()
+	{
+		string[] S = Console.ReadLine().Split();
+		N = int.Parse(S[0]);
+		M = int.Parse(S[1]);
+		int k = int.Parse(S[2]);
+		T = new int[N, M];
+		V = new bool[N, M];
+		for (int i = 0; i < k; i++)
+		{
+			S = Console.ReadLine().Split();
+			T[int.Parse(S[0]), int.Parse(S[1])] = 1;
+		}
+		Push(Create(0, 0, 0));
+
+		Console.WriteLine(BFS());
+	}
+}
