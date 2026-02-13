da6dfdff405bfefe2a5da47504d27b3ab3a22d35
returnminusone <returnminusone@outlook.com>
2026-02-13 19:03:35 +0900

    add 25416

---
+++ 25416/25416.cs
@@ -0,0 +1,81 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618
+
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+	public class Pos
+	{
+		public int x;
+		public int y;
+		public int w;
+
+		public Pos(int x, int y, int w)
+		{
+			this.x = x;
+			this.y = y;
+			this.w = w;
+		}
+	}
+
+	public static Pos Create(int x, int y, int w)
+	{
+		if (x < 0 || x >= 5 || y < 0 || y >= 5) return null;
+		if (T[x, y] == -1) return null;
+		if (V[x, y]) return null;
+
+		return new Pos(x, y, w);
+	}
+
+	public static bool Push(Pos pos)
+	{
+		if (pos == null) return false;
+
+		Q.Enqueue(pos);
+		V[pos.x, pos.y] = true;
+
+		return true;
+	}
+
+	public static int BFS()
+	{
+		while (Q.Count > 0)
+		{
+			Pos p = Q.Dequeue();
+
+			if (T[p.x, p.y] == 1) return p.w;
+
+			for (int i = 0; i < 4; i++)
+			{
+				Push(Create(p.x + dx[i], p.y + dy[i], p.w + 1));
+			}
+		}
+
+		return -1;
+	}
+
+	public static int[,] T;
+	public static bool[,] V;
+	public static Queue<Pos> Q = new Queue<Pos>();
+
+	public static int[] dx = { -1, 0, 1, 0 };
+	public static int[] dy = { 0, -1, 0, 1 };
+
+	public static void Main(string[] args)
+	{
+		T = new int[5, 5];
+		for (int i = 0; i < 5; i++)
+		{
+			string[] S = Console.ReadLine().Split();
+			for (int j = 0; j < 5; j++)
+			{
+				T[i, j] = int.Parse(S[j]);
+			}
+		}
+		string[] s = Console.ReadLine().Split();
+		V = new bool[5, 5];
+		Push(Create(int.Parse(s[0]), int.Parse(s[1]), 0));
+
+		Console.WriteLine(BFS());
+	}
+}
