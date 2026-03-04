de4428a3d01cb58284e24eabcbfa2711421c1136
returnminusone <returnminusone@outlook.com>
2026-03-04 21:44:10 +0900

    add 14659

---
+++ 14659/14659.cs
@@ -0,0 +1,23 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static void Main()
+	{
+		int N = int.Parse(Console.ReadLine());
+		string[] S = Console.ReadLine().Split();
+		int R = 0, r = 0, left = int.Parse(S[0]);
+		for (int i = 1; i < N; i++)
+		{
+			if (left <= int.Parse(S[i]))
+			{
+				if (r > R) R = r;
+				left = int.Parse(S[i]);
+				r = 0;
+			}
+			else r++;
+		}
+
+		Console.WriteLine(R < r ? r : R);
+	}
+}
