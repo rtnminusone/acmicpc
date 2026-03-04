be77c1baef7359b28689b1ac503131e26b02d2af
returnminusone <returnminusone@outlook.com>
2026-03-04 21:48:45 +0900

    add 28701

---
+++ 28701/28701.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static void Main()
+	{
+		int N = int.Parse(Console.ReadLine());
+		N = N * (N + 1) / 2;
+
+		Console.WriteLine(N + "\n" + (N * N) + "\n" + (N * N));
+	}
+}
