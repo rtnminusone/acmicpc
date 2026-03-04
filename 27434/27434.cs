4904a9157ace91b6b202a590809ddcb21d596340
returnminusone <returnminusone@outlook.com>
2026-03-04 22:03:10 +0900

    add 27434

---
+++ 27434/27434.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+using System.Numerics;
+
+class Program
+{
+	public static BigInteger Factorial(int A, int N)
+	{
+		BigInteger R = new BigInteger(A);
+
+		if (A < N)
+		{
+			int B = (A + N) / 2;
+			R = Factorial(A, B) * Factorial(B + 1, N);
+		}
+
+		return R;
+	}
+
+	public static void Main()
+	{
+		Console.WriteLine(Factorial(1, int.Parse(Console.ReadLine())));
+	}
+}
