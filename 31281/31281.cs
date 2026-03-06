6cd2d709880c1fc6053f3114caebe812c667a321
returnminusone <returnminusone@outlook.com>
2026-03-06 17:10:01 +0900

    add 31281

---
+++ 31281/31281.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+PriorityQueue<long, long> PQ = new PriorityQueue<long, long>();
+
+string[] S = Console.ReadLine().Split();
+for (int i = 0; i < 3; i++)
+{
+	long k = long.Parse(S[i]);
+	PQ.Enqueue(k, k);
+}
+PQ.Dequeue();
+
+Console.WriteLine(PQ.Dequeue());
