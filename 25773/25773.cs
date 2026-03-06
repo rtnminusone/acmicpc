910f7c8db5fface1c086985507fe630fd2bfe691
returnminusone <returnminusone@outlook.com>
2026-03-06 18:52:30 +0900

    add 25773

---
+++ 25773/25773.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+PriorityQueue<char, int> PQ = new PriorityQueue<char, int>();
+
+string S = Console.ReadLine();
+for (int i = 0; i < S.Length; i++)
+{
+	PQ.Enqueue(S[i], -S[i]);
+}
+
+for (int i = 0; i < S.Length; i++)
+{
+	Console.Write(PQ.Dequeue());
+}
