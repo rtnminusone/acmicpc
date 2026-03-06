92ed2f254d1c61fa52e63aad2f80ae1d7a3d8f79
returnminusone <returnminusone@outlook.com>
2026-03-06 16:17:31 +0900

    add 1074

---
+++ 1074/1074.cs
@@ -0,0 +1,38 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static void Main()
+	{
+		string[] S = Console.ReadLine().Split();
+		int N = int.Parse(S[0]);
+		int r = int.Parse(S[1]);
+		int c = int.Parse(S[2]);
+		int R = 0;
+		for (int i = N - 1; i >= 0; i--)
+		{
+			int half = 1 << i;
+			int area = half * half;
+
+			if (r < half && c < half) continue;
+			else if (r < half && c >= half)
+			{
+				R += area;
+				c -= half;
+			}
+			else if (r >= half && c < half)
+			{
+				R += area * 2;
+				r -= half;
+			}
+			else
+			{
+				R += area * 3;
+				r -= half;
+				c -= half;
+			}
+		}
+
+		Console.WriteLine(R);
+	}
+}
