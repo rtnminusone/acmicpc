1340b8642de3cbce890082a3a42041896d92c298
returnminusone <returnminusone@outlook.com>
2026-03-07 19:55:11 +0900

    add 2204

---
+++ 2204/2204.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+PriorityQueue<string, string> PQ = new PriorityQueue<string, string>();
+
+while (true)
+{
+	PQ.Clear();
+	int N = int.Parse(Console.ReadLine());
+	if (N == 0) break;
+	for (int i = 0; i < N; i++)
+	{
+		string S = Console.ReadLine();
+		PQ.Enqueue(S, S.ToLower());
+	}
+
+	Console.WriteLine(PQ.Dequeue());
+}
