a6753c2186b8bb42c5666aa33fc84e8d3b67698e
returnminusone <returnminusone@outlook.com>
2026-03-07 20:22:09 +0900

    add 23882

---
+++ 23882/23882.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+string[] S = Console.ReadLine().Split();
+int N = int.Parse(S[0]);
+int K = int.Parse(S[1]);
+int[] T = new int[N];
+S = Console.ReadLine().Split();
+for (int i = 0; i < N; i++)
+{
+	T[i] = int.Parse(S[i]);
+}
+int k = 0;
+for (int i = N - 1; i > 0; i--)
+{
+	int idx = 0;
+	for (int j = 0; j <= i; j++)
+	{
+		if (T[idx] < T[j]) idx = j;
+	}
+	if (idx != i)
+	{
+		k++;
+		int tmp = T[i];
+		T[i] = T[idx];
+		T[idx] = tmp;
+		if (K == k)
+		{
+			Console.WriteLine(string.Join(" ", T));
+			Environment.Exit(0);
+		}
+	}
+}
+
+Console.WriteLine(-1);
