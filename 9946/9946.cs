5e422029dd6f0200f53d19d34e6460f31188e3ad
returnminusone <returnminusone@outlook.com>
2026-03-19 22:59:33 +0900

    add 9946

---
+++ 9946/9946.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+using System.Text;
+
+StringBuilder sb = new StringBuilder();
+int idx = 1;
+
+while (true)
+{
+	string S1 = Console.ReadLine();
+	string S2 = Console.ReadLine();
+	if (S1.Equals("END") && S1.Equals(S2)) break;
+	sb.Append("Case " + (idx++) + ": ");
+	if (S1.Length != S2.Length) sb.AppendLine("different");
+	else
+	{
+		char[] C1 = S1.ToCharArray();
+		char[] C2 = S2.ToCharArray();
+		Array.Sort(C1);
+		Array.Sort(C2);
+		if (new String(C1).Equals(new String(C2))) sb.AppendLine("same");
+		else sb.AppendLine("different");
+	}
+}
+
+Console.WriteLine(sb);
