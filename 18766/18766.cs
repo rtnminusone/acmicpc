26445c1f1bab3beab3843957c1493186e007bd51
returnminusone <returnminusone@outlook.com>
2026-03-22 02:03:31 +0900

    add 18766

---
+++ 18766/18766.cs
@@ -0,0 +1,34 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+using System.Text;
+
+class Program
+{
+	public static void Main()
+	{
+		StringBuilder sb = new StringBuilder();
+
+		int T = int.Parse(Console.ReadLine());
+		while (T-- > 0)
+		{
+			int N = int.Parse(Console.ReadLine());
+			string[] A = Console.ReadLine().Split();
+			string[] B = Console.ReadLine().Split();
+			Array.Sort(A);
+			Array.Sort(B);
+			bool flg = true;
+			for (int i = 0; i < N; i++)
+			{
+				if (A[i] != B[i])
+				{
+					flg = false;
+					break;
+				}
+			}
+
+			sb.AppendLine(flg ? "NOT CHEATER" : "CHEATER");
+		}
+
+		Console.WriteLine(sb.ToString());
+	}
+}
